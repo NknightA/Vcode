68d8ea5acad36336062ebf93f8f2e01153314b06
NknightA <n.knight.pc0627@gmail.com>
2022-07-08 09:16:57 +0900

    new UI.

--- Vcode5/src/navigations/index.js
+++ Vcode5/src/navigations/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
-import { StyleSheet } from 'react-native';
-
+import { Button, StyleSheet } from 'react-native';
 import HomeScreen from '../screens/HomeScreen';
 import DetailScreen from '../screens/DetailScreen';
 
@@ -13,20 +12,21 @@ const RootStack = createStackNavigator();
 const RootStackScreeen = () => {
   return (
     
-    <NavigationContainer>
-   
-      <RootStack.Navigator initialRouteName='top' screenOptions={{ headerStyle: styles.header}}>
-      <RootStack.Screen name='Home' component={HomeScreen} options={{ title: 'My dev' }}/>
-      <RootStack.Screen name='Detail' component={DetailScreen}/>
+    <NavigationContainer> 
+   <RootStack.Navigator initialRouteName='top' screenOptions={{ headerStyle: styles.header}}>
+      <RootStack.Screen name='Home' component={HomeScreen} />
+      <RootStack.Screen name='Detail' component={DetailScreen} />
       </RootStack.Navigator>
     </NavigationContainer>
   )
 }
+
+
+
 const styles = StyleSheet.create({
   header: {
-    borderBottomWidth: 3,
-    borderBottomColor: 'blue',
- 
+    backgroundColor: 'black',
+    height: 0
   },
 });
 
--- Vcode5/src/screens/HomeScreen.js
+++ Vcode5/src/screens/HomeScreen.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import {StyleSheet, View, Text, Button} from 'react-native';
+import {StyleSheet, View, Text, Button, TouchableOpacity} from 'react-native';
 import DetailScreen from './DetailScreen';
 
-
+//prottype positioning
+//Accunt Button
 
 const HomeScreen = ({navigation}) => {
 
@@ -11,60 +12,226 @@ const HomeScreen = ({navigation}) => {
       
       <View style={styles.containerBox1}>
         <Text style={styles.Text_l}>Hello! Developer!</Text>
+        <View style={styles.AccoutButton}>
+        <View style={styles.Button}>
+            <TouchableOpacity style={styles.AccountIconButton}>
+              <Text>aaa</Text>
+            </TouchableOpacity>        
+            </View>   
+            
+        </View>
+        <View style={styles.ButtonBox}>
+            
+            
+            <View style={styles.Button2}>
+            <TouchableOpacity style={styles.ButtonText3}>
+              <Text>aaa</Text>
+            </TouchableOpacity>        
+            </View>   
+
+            <View style={styles.Button2}>
+            <TouchableOpacity style={styles.ButtonText3}>
+              <Text>aaa</Text>
+            </TouchableOpacity>        
+            </View>   
+          
+        
+      </View>   
+
       <View style={styles.ButtonBox}>
-          <View style={styles.Button}>
-            <Button
-                title="aaa"
-                onPress={() => {
-                navigation.navigate('Detail');
-               }}
-             />
-            </View>
+            
+            
+            <View style={styles.Button2}>
+            <TouchableOpacity style={styles.ButtonText3}>
+              <Text>aaa</Text>
+            </TouchableOpacity>        
+            </View>   
 
-            <View style={styles.Button}>
+            <View style={styles.Button2}>
+            <TouchableOpacity style={styles.ButtonText3}>
+              <Text>aaa</Text>
+            </TouchableOpacity>        
+            </View>   
+          
+        
+      </View>   
+      <View style={styles.ButtonBox}>
+            
+            
+            <View style={styles.Button2}>
+            <TouchableOpacity style={styles.ButtonText3}>
+              <Text>aaa</Text>
+            </TouchableOpacity>        
+            </View>   
 
-              <Button
-                title="bbb"
-                onPress={() => {
-                navigation.navigate('Detail');
-              }}
-              />
-            </View>
-          </View>  
+            <View style={styles.Button2}>
+            <TouchableOpacity style={styles.ButtonText3}>
+              <Text>aaa</Text>
+            </TouchableOpacity>        
+            </View>   
+          
+        
+      </View>
+
+        <View style={styles.ButtonBox3}>
+            
+            
+            <View style={styles.Button4}>
+            <TouchableOpacity style={styles.ButtonText4}>
+              <Text>aaa</Text>
+            </TouchableOpacity>        
+            </View>   
+
+            <View style={styles.Button4}>
+            <TouchableOpacity style={styles.ButtonText4}>
+              <Text>aaa</Text>
+            </TouchableOpacity>        
+            </View>  
+        
+      </View> 
+
+        <View style={styles.ButtonBox4}>
+            <View style={styles.Button4}>
+            <TouchableOpacity style={styles.ButtonText4}>
+              <Text>aaa</Text>
+            </TouchableOpacity>        
+            </View>   
+
+            <View style={styles.Button4}>
+            <TouchableOpacity style={styles.ButtonText4}>
+              <Text>aaa</Text>
+            </TouchableOpacity>        
+            </View>  
+        
       </View>        
-    </View>       
+      </View>
+  </View>       
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'yellow',
+    backgroundColor: 'black',
+    marginTop: 30
   },
 
   containerBox1 : {
     flex: 0.5,
     backgroundColor: 'black',
-    alignItems : 'center'
+    alignItems : 'center',
   },
 
   ButtonBox:{
+    width : '70%',
+    flexDirection: 'row',
+    alignItems : 'flex-end',
+    justifyContent : 'center',
+    height : 45,
+    borderRadius: 5,
+  },
+
+  AccoutButton:{
     marginTop: 10,
     width : '100%',
     flexDirection: 'row',
     alignItems : 'center',
-    backgroundColor :'skyblue',
+    justifyContent : 'center',
+    padding :10
   },
 
-  Button : {
-    width: 100,
-    height : 200,
-    margin : 10,
+  ButtonBox2:{
+    margin: 50,
+    width : '100%',
+    alignItems : 'center',
+    paddingLeft :30,
+    paddingRight :30,
   },
+
+  ButtonBox3:{
+    marginTop : 20 ,
+    width : '100%',
+    alignItems : 'center',
+    justifyContent: 'center' ,
+    flexDirection: 'row',
+  },
+
+  ButtonBox4:{
+    marginTop : 10 ,
+    width : '100%',
+    alignItems : 'center',
+    justifyContent: 'center' ,
+    flexDirection: 'row',
+  },
+
+  Button: {
+    margin : 10
+  },
+  
+  Button2: {
+    padding :10 
+  },
+
+  Button3: {
+    width: 200,
+    padding: 10 
+  },
+
+  Button4: {
+    padding :10 ,
+  },
+
+  AccountIconButton: {
+    backgroundColor: '#FFFF00',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width:100,
+    height: 100,
+    elevation: 2,
+    position: 'relative',
+    borderRadius:50,
+    
+  },
+  
+  ButtonText2: {
+    backgroundColor: '#FFFF00',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 500,
+    height: 100,
+    elevation: 2,
+    position: 'relative',
+    borderRadius:10,
+    
+  },
+
+  ButtonText3: {
+    backgroundColor: '#FFFF00',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width:100,
+    height: 35,
+    elevation: 2,
+    position: 'relative',
+    borderRadius:5,
+  },
+
+  ButtonText4: {
+    backgroundColor: '#FFFF00',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width:150,
+    height: 150,
+    elevation: 2,
+    position: 'relative',
+    borderRadius:3,
+    
+  },
+
 
   Text_l : {
     color: 'white',
-    fontSize: 24 ,
+    fontSize: 40 ,
   },
 
   Text_m : {
