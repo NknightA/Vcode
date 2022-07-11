08a9e7fb4aae0fd7d996354a2f91e66c2a8b39e4
NknightA <n.knight.pc0627@gmail.com>
2022-07-11 14:54:18 +0900

    changed directry name.

---
+++ Vcode/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import RootStackScreen from './src/navigations/';
+import {StyleSheet, View, Text, Button} from 'react-native';
+
+const App = () => <RootStackScreen />;
+
+export default App;
+
