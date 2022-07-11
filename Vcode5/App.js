fe95c3a2fb954060ed856da1ca5fc4e7207475e9
NknightA <88763245+NknightA@users.noreply.github.com>
2022-07-11 14:56:49 +0900

    Delete Vcode5 directory

--- Vcode5/App.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import RootStackScreen from './src/navigations/';
-import {StyleSheet, View, Text, Button} from 'react-native';
-
-const App = () => <RootStackScreen />;
-
-export default App;
-
