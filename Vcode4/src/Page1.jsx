16e97ec65005359b656b8d58abe1ee05d35e271e
NknightA <n.knight.pc0627@gmail.com>
2022-07-11 11:13:23 +0900

    delete files.

--- Vcode4/src/Page1.jsx
+++
@@ -1,9 +0,0 @@
-import { Link } from "react-router-dom";
-
-export const Page1 = () => {
-    return(
-        <div>
-            <h1>Page1</h1>
-        </div>
-    );
-}
--- Vcode4/src/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReacrDOM from "react-dom";
-import App from "./App";
-
-ReacrDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
-)
